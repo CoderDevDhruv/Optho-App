9c655dee4c3a2973150b01b59b1d856063059041
CoderDevDhruv <144376567+CoderDevDhruv@users.noreply.github.com>
2025-01-12 23:50:39 +0530

    Update index.js
    port changed

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ google auth
 in index.js and their respective file and patientDet.ejs*/
 
 const app = express();
-const port = 3000;
+const port = process.env.port;
 const saltRounds = 5;
 
 app.use(bodyParser.urlencoded({ extended: true }));
